c2854ce2a549b55913d5987ec606bda96b9bef98
OskarKlintrot <oskar.klintrot@gmail.com>
2015-12-17 19:35:44 +0100

    Moved source to src again and added eslint and minifying to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,82 @@
+var webpack = require('webpack')
+var path = require('path')
+var buildPath = path.resolve(__dirname, '')
+var sourcePath = path.resolve(__dirname, 'src')
+var nodeModulesPath = path.resolve(__dirname, 'node_modules')
+var TransferWebpackPlugin = require('transfer-webpack-plugin')
+
+var production = process.argv.find((element) => element === '--production');
+
 var config = {
     entry: {
-        js: [
-          'babel-polyfill',
-          './src/app/app.jsx'
-        ],
-        html: './index.html',
+      js: [
+        'babel-polyfill',
+        './src/app/app.jsx',
+      ],
+      html: './src/www/index.html',
+    },
+    devServer:{
+      contentBase: 'src/www',
+      devtool: 'source-map',
+      hot: true,
+      inline: true,
+      port: 3000,
     },
     output: {
-        path: __dirname,
-        filename: 'app.js',
+        path: buildPath,
+        filename: 'boundle.min.js',
     },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin(),
+      new TransferWebpackPlugin([
+        {from: 'www'},
+      ], sourcePath),
+    ],
     module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loaders: [
-                    'react-hot',
-                    'babel?' + JSON.stringify({
-                        presets: ["react", "es2015", "stage-1"],
-                    })
-                ],
-            },
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                loaders: [
-                    'react-hot',
-                    'babel?' + JSON.stringify({
-                        presets: ["react", "es2015", "stage-1"],
-                    })
-                ],
-            },
-            {
-                test: /\.html$/,
-                loader: "file?name=[name].[ext]",
-            },
-        ],
+      preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'eslint-loader',
+        include: [path.resolve(__dirname, "src/app")],
+        exclude: [nodeModulesPath],
+      },
+    ],
+    loaders: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: nodeModulesPath,
+          loaders: [
+              'react-hot',
+              'babel?' + JSON.stringify({
+                  presets: ["react", "es2015", "stage-1"],
+              }),
+          ],
+        },
+        {
+          test: /\.html$/,
+          loader: "file?name=[name].[ext]",
+        },
+      ],
     },
     resolve: {
-        extensions: ['', '.js', '.jsx'],
-        root: __dirname,
+      extensions: ['', '.js', '.jsx'],
+      root: __dirname,
     },
     devtool: 'source-map',
-};
-module.exports = config;
+    eslint: {
+      configFile: '.eslintrc',
+    },
+}
+
+if (production) {
+  config.plugins = [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+  ].concat(config.plugins)
+}
+
+module.exports = config
