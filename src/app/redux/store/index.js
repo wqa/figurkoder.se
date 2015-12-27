e46b7cc821bfbb50ab92ecfaf52db48c098f695a
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-12-27 11:08:45 +0100

    Rename

--- src/app/redux/store/index.js
+++ src/app/redux/store/index.js
@@ -2,7 +2,7 @@ import Constants from '../constants'
 
 // if (process.argv.find((element) => element === '--production')) {
 if (Constants.PRODUCTION) {
-  module.exports = require('./store.prod')
+  module.exports = require('./configureStore.prod')
 } else {
-  module.exports = require('./store.dev')
+  module.exports = require('./configureStore.dev')
 }
